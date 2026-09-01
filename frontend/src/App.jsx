6f140f00ed4ba9a101709f76b4701a718db6ac6e
amankumar-polaris <aman.kumar@polariscampus.com>
2026-09-01 15:09:49 +0530

    add: frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import React,{useState,useEffect,useRef} from 'react'
+
+// const App = () => {
+//   // let count = 0
+//   const[count,setCount] = useState(0)
+//   // setCount(1)
+
+//   // const handleClick = ()=>{setCount(prev=>prev+1)
+//   //   console.log(count)
+//   // }
+//   setCount(prev=>prev+1)
+
+//   // console.log(count,'count') //0=>8,1=>2
+//   return (
+//     <div>
+//       <h1>{count}</h1> 
+//       {/* 0=>9, 1=>0 */}
+//       {/* <button onClick = {handleClick}>click me</button> */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+// const App = () => {
+//   const[count,setCount] = useState(0)
+//   const[value,setValue] = useState(0)
+//   useEffect(()=>{
+//     console.log(count,'count')
+//     console.log(value,'value')
+//   },[count])
+  
+//   return (
+//     <div>
+//       {/* <h1>{count}</h1> */}
+//       <button onClick={()=>setCount(prev=>prev+1)}>Count</button>
+//       <button onClick={()=>setValue(prev=>prev+1)}>Value</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// const App = () => {
+//   const inputRef = useRef(null)
+
+//   const handleInput = ()=>{
+//     inputRef.current.focus()
+
+//   }
+//   return (
+//     <div>
+//       <input type="text" onChange = {handleInput} ref = {inputRef}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
